85a54b8505b320c38b1f56cac9329d7b2909ea0c
paulapompeo <paulapompeoc@gmail.com>
2021-11-09 12:05:33 +0100

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,40 +1,40 @@
 const request = require('request')
 
-const postWebhook = (req, res) =>{
+const postWebhook = (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
 
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-      // Iterate over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function (entry) {
 
-          // Gets the body of the webhook event
-          let webhook_event = entry.messaging[0];
-          console.log(webhook_event);
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
 
 
-          // Get the sender PSID
-          let sender_psid = webhook_event.sender.id;
-          console.log('Sender PSID: ' + sender_psid);
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
 
-          // Check if the event is a message or postback and
-          // pass the event to the appropriate handler function
-          if (webhook_event.message) {
-              handleMessage(sender_psid, webhook_event.message);
-          } else if (webhook_event.postback) {
-              handlePostback(sender_psid, webhook_event.postback);
-          }
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback);
+      }
 
-      });
+    });
 
-      // Return a '200 OK' response to all events
-      res.status(200).send('EVENT_RECEIVED');
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
 
   } else {
-      // Return a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
   }
 };
 
@@ -75,6 +75,35 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message
     response = {
       "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  } else if (received_message.attachments) {
+
+    // Gets the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
     }
   }
 
