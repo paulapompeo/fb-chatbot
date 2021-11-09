0d246a30995a0af4e9a048092713ecf91ff93739
paulapompeo <paulapompeoc@gmail.com>
2021-11-09 11:58:20 +0100

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,30 +1,42 @@
 const request = require('request')
 
-const postWebhook = (req, res) => {
+const postWebhook = (req, res) =>{
   // Parse the request body from the POST
   let body = req.body;
 
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-    // Gets the body of the webhook event
-    let webhook_event = entry.messaging[0];
-    console.log(webhook_event);
+      // Iterate over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+
+          // Gets the body of the webhook event
+          let webhook_event = entry.messaging[0];
+          console.log(webhook_event);
 
 
-    // Get the sender PSID
-    let sender_psid = webhook_event.sender.id;
-    console.log('Sender PSID: ' + sender_psid);
+          // Get the sender PSID
+          let sender_psid = webhook_event.sender.id;
+          console.log('Sender PSID: ' + sender_psid);
 
-    // Check if the event is a message or postback and
-    // pass the event to the appropriate handler function
-    if (webhook_event.message) {
-      handleMessage(sender_psid, webhook_event.message);
-    } else if (webhook_event.postback) {
-      handlePostback(sender_psid, webhook_event.postback);
-    }
+          // Check if the event is a message or postback and
+          // pass the event to the appropriate handler function
+          if (webhook_event.message) {
+              handleMessage(sender_psid, webhook_event.message);
+          } else if (webhook_event.postback) {
+              handlePostback(sender_psid, webhook_event.postback);
+          }
+
+      });
+
+      // Return a '200 OK' response to all events
+      res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+      // Return a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
   }
-}
+};
 
 const getWebhook = (req, res) => {
 
